43217d7df348610ce7dc87a0c0f892f8d087c82f
Anjlnobita <jiteshkumar7200@gmail.com>
2024-12-20 17:36:30 +0530

    Update __init__.py

--- HinataHyuga/modules/sql/__init__.py
+++ HinataHyuga/modules/sql/__init__.py
@@ -1,1 +1,19 @@
- 
+# MongoDB Connection Setup
+
+from pymongo import MongoClient
+from DazaiRobot import DB_URI
+from DazaiRobot import LOGGER as log
+
+def start():
+    client = MongoClient(DB_URI)
+    log.info("[MongoDB] Connecting to database......")
+    return client
+
+try:
+    client = start()
+    nobita = client.get_default_database()
+except Exception as e:
+    log.exception(f"[MongoDB] Failed to connect due to {e}")
+    exit()
+
+log.info("[MongoDB] Connection successful, client started.")
